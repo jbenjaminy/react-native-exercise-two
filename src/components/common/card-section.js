7af5dd3c586db780c00c073a41b10722d04639bb
jbenjaminy <young.benjamin@gmail.com>
2017-01-26 23:42:46 -0600

    adds content to card-section.js

---
+++ src/components/common/card-section.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const CardSection = (props) => (
+        <View style={styles.containerStyle}>
+            {props.children}
+        </View>
+    );
+
+const styles = {
+    containerStyle: {
+    // borders & coloring
+        borderBottomWidth: 1,
+        borderColor: '#ddd',
+        backgroundColor: '#fff',
+    // spacing & orientation
+        padding: 5,
+        justifyContent: 'flex-start',
+        flexDirection: 'row',
+        position: 'relative'
+    }
+};
+
+export default CardSection;
