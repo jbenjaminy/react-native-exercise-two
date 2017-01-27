168c27498b1181f9f682d78304473f0b10216843
jbenjaminy <young.benjamin@gmail.com>
2017-01-27 00:14:29 -0600

    fixes import statement and updates comments

--- index.android.js
+++ index.android.js
@@ -1,6 +1,8 @@
-// All we want to do in both index.android.js & index.ios.js is to make use
-// of the App component, so that we can keep the app consistent between both
-// platforms. First place we are doing any logic/modifications is app.js file.
+/*
+All we want to do in both index.android.js & index.ios.js is to make use of the
+App component, so that we can keep the app consistent between both platforms.
+First place we are doing any logic/modifications is app.js file.
+*/
 import { AppRegistry } from 'react-native';
 import App from './src/app';
 
