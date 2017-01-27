4916c426b7bb49a7925c001622bfdf9313a12148
jbenjaminy <young.benjamin@gmail.com>
2017-01-26 23:44:58 -0600

    adds content to button.js

---
+++ src/components/common/button.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+
+const Button = ({ onPress }) => {
+    const { buttonStyle, textStyle } = styles;
+    return (
+        <TouchableOpacity
+            onPress={onPress}
+            style={buttonStyle}
+        >
+            <Text style={textStyle}>Purchase</Text>
+        </TouchableOpacity>
+    );
+};
+
+const styles = {
+    buttonStyle: {
+    // sizing
+        flex: 1,
+    // layout
+        alignSelf: 'stretch',
+    // borders
+        borderWidth: 1,
+        borderRadius: 5,
+    // coloring
+        // close to iOS blue
+        borderColor: '#007aff',
+        backgroundColor: '#fff',
+    // spacing
+        marginLeft: 5,
+        marginRight: 5
+    },
+    textStyle: {
+    // layout
+        alignSelf: 'center',
+    // coloring & effects
+        color: '#007aff',
+        fontWeight: '600',
+    // sizing
+        fontSize: 16,
+    // spacing
+        paddingTop: 10,
+        paddingBottom: 10
+    }
+};
+
+export default Button;
