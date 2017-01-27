77bb49c7de34ae10fd5df2ecda56debe65ec78f6
jbenjaminy <young.benjamin@gmail.com>
2017-01-26 23:43:45 -0600

    adds content to card-container.js

---
+++ src/components/common/card-container.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const CardContainer = (props) => (
+        <View style={styles.viewStyle}>
+            {props.children}
+        </View>
+);
+
+const styles = {
+    viewStyle: {
+    // borders
+        borderWidth: 1,
+        borderRadius: 2,
+        borderColor: '#ddd',
+        borderBottomWidth: 0,
+    // shadowing
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 2,
+    // spacing
+        marginLeft: 5,
+        marginRight: 5,
+        marginTop: 10,
+    // other styling
+        elevation: 1
+    }
+};
+
+export default CardContainer;
