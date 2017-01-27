48ee6e12c95c679832bcbd25d19af780442db7fd
jbenjaminy <young.benjamin@gmail.com>
2017-01-26 23:58:24 -0600

    creates index.js file for common directory

---
+++ src/components/common/index.js
@@ -0,0 +1,4 @@
+// This file is used to aggregate our components for ease of importing and
+// reusability. We can just import the components from the common directory,
+// and whenever you import from the directory directly, without specifying
+// a filename, it will by default look to the index.js file.
