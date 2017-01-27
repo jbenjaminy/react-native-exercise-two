5fd7d3b8551a5947a0fa509b24bc5af360cae64d
jbenjaminy <young.benjamin@gmail.com>
2017-01-27 00:52:07 -0600

    adds firebase module to package.json and adds firebase initialization in app component

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+    /*
+        Use lifecycle method for firebase config, before app is rendered.
+    */
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyDn42UXaXnYi5r08wMuDS-YmQEk57aCQsw',
+            authDomain: 'react-native-auth-1ea19.firebaseapp.com',
+            databaseURL: 'https://react-native-auth-1ea19.firebaseio.com',
+            storageBucket: 'react-native-auth-1ea19.appspot.com',
+            messagingSenderId: '939471548555'
+        });
+    }
+
     render() {
         return (
             <View>
