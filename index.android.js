688c7daa129fc7f37c26db792fc9217a648fcd27
jbenjaminy <young.benjamin@gmail.com>
2017-01-26 23:29:43 -0600

    duplicates index.ios.js content in index.android.js

--- index.android.js
+++ index.android.js
@@ -1,2 +1,7 @@
-// All we want to do in both index.android.js & index.ios.js is render the App
-// component, so that we can keep the app consistent between both platforms.
+// All we want to do in both index.android.js & index.ios.js is to make use
+// of the App component, so that we can keep the app consistent between both
+// platforms. First place we are doing any logic/modifications is app.js file.
+import { AppRegistry } from 'react-native';
+import App from './src/app';
+
+AppRegistry.registerComponent('auth', () => App);
