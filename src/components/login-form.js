695639d75989e8492cce364e758e7e006051bece
jbenjaminy <young.benjamin@gmail.com>
2017-01-27 01:10:46 -0600

    adds TextInput tag to LoginForm component

--- src/components/login-form.js
+++ src/components/login-form.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+/* TextInput is another 'primative' tag from react-native */
+import { TextInput } from 'react-native';
 import { Button, CardContainer, CardSection } from './common';
 
 class LoginForm extends Component {
     render() {
         return (
             <CardContainer>
-                <CardSection />
+                <CardSection>
+                    {/*
+                        TextInput tags behave like the Image tags -- they don't
+                        have a height/width set to them and require this to
+                        render. Otherwise they render with height/width of 0;
+                    */}
+                    <TextInput style={{ height: 20, width: 100 }} />
+                </CardSection>
                 <CardSection />
                 <CardSection>
                     <Button>
