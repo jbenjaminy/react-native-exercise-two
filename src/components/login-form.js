8dd49c00995493c068b418d75bdd2ae4db1d6c3f
jbenjaminy <young.benjamin@gmail.com>
2017-01-31 00:45:18 -0600

    adds state to LoginForm component

--- src/components/login-form.js
+++ src/components/login-form.js
@@ -4,6 +4,9 @@ import { TextInput } from 'react-native';
 import { Button, CardContainer, CardSection } from './common';
 
 class LoginForm extends Component {
+    /* state is what we use to deal with feedback from the user. */
+    state = { text: '' };
+
     render() {
         return (
             <CardContainer>
@@ -12,8 +15,21 @@ class LoginForm extends Component {
                         TextInput tags behave like the Image tags -- they don't
                         have a height/width set to them and require this to
                         render. Otherwise they render with height/width of 0;
+
+                        TextInput => User types text => 'onChangeText' callback
+                        => 'setState' with new text => component rerenders =>
+                        when TextInput rerenders, we tell it that its value
+                        property is 'this.state.text'
+
+                        -fat arrow callback function gets called whenever a
+                        user types in text -- gets called with the value the
+                        user added.
                     */}
-                    <TextInput style={{ height: 20, width: 100 }} />
+                    <TextInput
+                        value={this.state.text}
+                        onChangeText={text => this.setState({ text })}
+                        style={{ height: 20, width: 100 }}
+                    />
                 </CardSection>
 
                 <CardSection />
